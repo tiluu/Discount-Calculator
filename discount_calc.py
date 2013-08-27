4f3d915c152cd64ac787034a6c7fcbca8abd7761
tiluu <tina.p.luu@gmail.com>
2013-08-26 18:25:52 -0700

    milestone1

--- discount_calc.py
+++ discount_calc.py
@@ -1,3 +1,4 @@
 class DiscountCalculator(object):
 	def calculate(self, total, discount_amount, disocunt_type):
 		return 10.0
+
--- discount_calc_tests.py
+++ discount_calc_tests.py
@@ -1,8 +1,9 @@
 import unittest
-from discount_calc import DiscountCalculator 
+from discount_calc import DiscountCalculator
+
 
 class DiscountCalculatorTests(unittest.TestCase):
 	def test_ten_percent_discount(self):
 		discount_calculator = DiscountCalculator()
-		result = discount_calculator.calculate(100, 100, 'percent')
+		result = discount_calculator.calculate(100,10,'percent')
 		self.assertEqual(10.0, result)
