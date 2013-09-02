055b3071760ba554e540ba1608d296a158ab31e5
tiluu <tina.p.luu@gmail.com>
2013-09-02 06:05:23 -0700

    update

--- discount_calc.py
+++ discount_calc.py
@@ -1,4 +1,13 @@
 class DiscountCalculator(object):
-	def calculate(self, total, discount_amount, disocunt_type):
-		return 10.0
+	def calculate(self, total, discount_amount, discount_type):
+		if discount_type == 'percent':
+			if discount_amount > 100:
+				raise ValueError('Percentage discount cannot be greater than 100%')
+			percentage_discount = float(discount_amount)/100
+			discount = float(total)* percentage_discount
+		elif discount_type == 'absolute':
+			discount = discount_amount
+		else:
+			raise ValueError('Invalid discount type')
+		return discount
 
--- discount_calc_tests.py
+++ discount_calc_tests.py
@@ -7,3 +7,21 @@ class DiscountCalculatorTests(unittest.TestCase):
 		discount_calculator = DiscountCalculator()
 		result = discount_calculator.calculate(100,10,'percent')
 		self.assertEqual(10.0, result)
+	def test_fifteen_percent_discount(self):
+		discount_calculator = DiscountCalculator()
+		result = discount_calculator.calculate(100, 15, 'percent')
+		self.assertEqual(15.0,result)
+	
+	def test_five_dollar_discount(self):
+		discount_calculator = DiscountCalculator()
+		result = discount_calculator.calculate(200, 5, 'absolute')
+		self.assertEqual(5, result)
+	
+	def test_invalid_discount(self):
+		discount_calculator = DiscountCalculator()
+		self.assertRaises(ValueError, discount_calculator.calculate, 250, 5, 'random')
+
+	def test_excess_discount(self):
+		discount_calculator = DiscountCalculator()
+		self.assertRaises(ValueError, discount_calculator.calculate, 250, 110, 'percent')
+	
